938739401717a24ef92104e2972f0dfa2e03a9fb
soby <sobhanmousavi121122@gmail.com>
2025-03-29 11:53:28 +0330

    change for test

--- apps/jobber-auth/src/app/app.service.ts
+++ apps/jobber-auth/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return { message: 'Hello APsjfjyfdsdI' };
+    return { message: 'Hello API' };
   }
 }
